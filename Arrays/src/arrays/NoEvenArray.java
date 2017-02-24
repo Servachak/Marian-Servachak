92c7f6f21690695aed86eeb373db65d19f2940bb
servachak <servachak088@gmail.com>
2017-02-24 20:25:17 +0200

    Виводимо тільки марні елементи масиву від 0 до 9

--- Arrays/src/arrays/NoEvenArray.java
+++ Arrays/src/arrays/NoEvenArray.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 public class NoEvenArray {
 
 	public static void main(String[] args) {
-		int array[] = {1,2,3,4,5,6,7,8,9,0};
+		int array[] = {1,2,3,4,5,6,7,9,0};
 		int b = 0;
 		for (int j = 0; j < array.length; j++) {
 			System.out.print(array[j]);
 		}
 		System.out.println();
 		
-		for (int i = 0; i < array.length; i+=2) {
+		for (int i = 1; i < array.length; i++) {
 			Arrays.sort(array);
+		if (array[i]%2 == 0)
 			System.out.print(array[i]);
 			
 		}
