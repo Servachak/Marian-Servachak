571faa6cf277b1e845a607ca075518cc2677aad8
Администратор <@XTREME-0A6QR4CU>
2017-04-11 21:38:35 +0300

    new

--- CinemaServachak/src/cinemaServachak/Time.java
+++ CinemaServachak/src/cinemaServachak/Time.java
@@ -1,11 +1,11 @@
 package cinemaServachak;
 
 public class Time {
- 
+
 	private int hour;
 	private int min;
-	
-	public Time(int hour,int min){
+
+	public Time(int hour, int min) {
 		this.hour = hour;
 		this.min = min;
 	}
@@ -15,11 +15,17 @@ public class Time {
 	}
 
 	public void setHour(int hour) {
-		if (hour >= 0 && hour <= 23){
-		this.hour = hour;
-		}else{
-			System.out.println("Day has 24 hours");
+		boolean trueHour = false;
+		while (!trueHour) {
+			if (hour >= 0 && hour <= 23) {
+				this.hour = hour;
+				trueHour = true;
+			} else {
+				System.out.println("Day has 24 hours");
+				System.out.println("Enter again");
+			}
 		}
+
 	}
 
 	public int getMin() {
@@ -27,10 +33,15 @@ public class Time {
 	}
 
 	public void setMin(int min) {
-		if(min >= 0 && min <= 59){
-			this.min = min;
-		}else{
-			System.out.println("Hour has 60 min");
+		boolean trueMin = false;
+		while (!trueMin) {
+			if (min >= 0 && min <= 59) {
+				this.min = min;
+				trueMin = true;
+			} else {
+				System.out.println("Hour has 60 min");
+				System.out.println("Enter again");
+			}
 		}
 	}
 
@@ -38,5 +49,5 @@ public class Time {
 	public String toString() {
 		return "Time [hour=" + hour + ", min=" + min + "]";
 	}
- 
+
 }
