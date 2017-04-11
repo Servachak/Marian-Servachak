1beb2a9d03aec1647ecc08b1333ed716460fd434
Администратор <@XTREME-0A6QR4CU>
2017-04-11 21:00:12 +0300

    новіший

--- MyClock/src/test/TectCloc.java
+++ MyClock/src/test/TectCloc.java
@@ -1,23 +1,22 @@
 package test;
 
-
 import java.util.Scanner;
 
 public class TectCloc {
 	static Scanner scanner = new Scanner(System.in);
 	int hour;
 	int min;
-	
-	public TectCloc(int hour, int min){
+
+	public TectCloc(int hour, int min) {
 		this.hour = hour;
 		this.min = min;
 	}
 
-	public void durationTime( int duration){
-		
+	public void durationTime(int duration) {
+
 		min = min + duration;
 		for (int i = 0; i <= 24; i++) {
-			
+
 			if (min > 60) {
 				hour = hour + (min / 60);
 				min = min % 60;
@@ -25,19 +24,21 @@ public class TectCloc {
 				break;
 			}
 		}
-		
+
 		if (min < 10) {
 			System.out.println(hour + " :" + 0 + "" + min);
 		} else {
 			System.out.println(hour + " :" + min);
-		}}
+		}
+	}
+
 	public static void main(String[] args) {
 
 		TectCloc cloc = new TectCloc(2, 0);
 		System.out.println("Enter duration");
 		int duration = scanner.nextInt();
 		cloc.durationTime(duration);
-		
+
 	}
 
 }
